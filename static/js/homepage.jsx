75fd2b78129e23ccc3651f8de8988ac9a37be316
Katrina Connors <koktakatrina@gmail.com>
2023-12-21 12:30:46 -0800

    homepage update

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,7 +1,13 @@
 'use strict';
 
 function Homepage() {
-  return <div>This component needs to be implemented.</div>;
+  return (
+    <div>
+      Welcome user! <br></br><br></br>
+      <a href="/cards">Cards Page</a> <br></br>
+      <img src="/static/img/balloonicorn.jpg"/>
+    </div>
+  );
 }
 
 ReactDOM.render(<Homepage />, document.querySelector('#app'));
